4c264eaff789df848b624e1f9d894ff05bc5525a
cadavUGent <54526345+cadavUGent@users.noreply.github.com>
2019-08-26 11:09:52 +0200

    Script for SAM to BAM conversion

--- RNA-Seq_Pipeline/Sam_to_Bam.sh
+++ RNA-Seq_Pipeline/Sam_to_Bam.sh
@@ -13,4 +13,12 @@ for i in {886..903}; do
 	samtools sort -o Unique.SRR952${i}.sorted Unique.SRR952${i}.bam
 done
 
+for i in {292..303}; do
+        samtools view -q 255 -Sb Aligned.SRR1121${i}.out.sam > Unique.SRR1121${i}.bam
+        samtools sort -o Unique.SRR1121${i}.sorted Unique.SRR1121${i}.bam
+done
 
+for i in {2991..3014}; do
+        samtools view -q 255 -Sb Aligned.SRR347${i}.sam > Unique.SRR347${i}.bam
+        samtools sort -o Unique.SRR347${i}.sorted Unique.SRR347${i}.bam
+done
